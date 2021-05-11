dd16891680064eef51c4658499dba05484d2f5db
unknown <20200649@s.ubaguio.edu>
2021-05-11 23:51:13 +0800

    hey queen

---
+++ site/Finals/External js/JavaScript/Script15.js
@@ -0,0 +1,25 @@
+
+var PerIn = "Personal Info";
+var P = PerIn.fontsize("5");
+document.writeln(P);
+document.writeln("        First Name                                          Charles Geyser J. Saliw-ey");
+document.writeln("        Age                                                 18");
+document.writeln("       ");
+document.writeln("       ");
+document.writeln("       ");
+var conIn = "     Contact Information";
+var C = conIn.fontsize("5");
+document.writeln(C)
+document.writeln("        Phone Number                                        09995709252");
+document.writeln("        Email Address                                       scharlesgeyser@gmail.com");
+document.writeln("        Mailing Address                                     Old Lucnab, Baguio City");
+document.writeln("       ");
+document.writeln("       ");
+document.writeln("       ");
+var acIn = "     Academic Information";
+var A = acIn.fontsize("5");
+document.writeln(A)
+document.writeln("        Program                                             Computer Science");
+document.writeln("        IDnumber                                            20200649");
+document.writeln("        Year Level                                          First Year");
+document.writeln("        Section                                             IAA1");
